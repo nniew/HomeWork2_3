a5ecfa6b22e92be6b92f3e722b76a7495d61d8f4
nniew <122811326+nniew@users.noreply.github.com>
2023-05-16 23:39:23 +0700

    Add files via upload

---
+++ HomeWork2_3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Program {
+  static void RunProgram(String step){
+    int m, n;
+    int.TryParse(Console.ReadLine(), out m);
+    int.TryParse(Console.ReadLine(), out n);
+    
+    
+    float[,] matrix1 = new float[m, n];
+    for(int i = 0; i < m; i++){
+      String line = Console.ReadLine();
+      String[] linesp = line.Split(' ');
+      for(int j = 0; j < n; j++){
+        float.TryParse(linesp[j], out matrix1[i, j]);
+      }
+    }
+    
+    float[,] matrix2 = new float[m, n];
+    for(int i = 0; i < m; i++){
+      String line = Console.ReadLine();
+      String[] linesp = line.Split(' ');
+      for(int j = 0; j < n; j++){
+        float.TryParse(linesp[j], out matrix2[i, j]);
+      }
+    }
+
+    float[,] result = new float[m, n];
+    for(int i = 0; i < m; i++){
+      for(int j = 0; j < n; j++){
+        if(step == "+"){
+          result[i,j] = matrix1[i, j] + matrix2[i, j];
+        }
+        else if(step == "-"){
+          result[i,j] = matrix1[i, j] - matrix2[i, j];
+        }
+        Console.Write(result[i, j]);
+        Console.Write(" ");
+      }
+      Console.WriteLine("");
+    }
+  }
+  
+  public static void Main (string[] args) {
+    while(true){
+      String step;
+      step = Console.ReadLine();
+      if (step == "-" || step == "+"){
+        RunProgram(step);
+        continue;
+      }
+      else{
+        break;
+      }
+    }
+  }
+}
